fcf799fa16aa909228a0afef556b2972d3f10fb7
Dave Syer <dsyer@gopivotal.com>
2014-03-28 13:12:20 +0000

    Update for Boot changes

--- src/test/java/demo/ApplicationTests.java
+++ src/test/java/demo/ApplicationTests.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.RestTemplates;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -14,10 +14,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @IntegrationTest
 public class ApplicationTests {
 
+	private TestRestTemplate restTemplate = new TestRestTemplate();
+
 	@Test
 	public void contextLoads() {
 		assertEquals("{\"message\":\"Hello World\"}",
-				RestTemplates.get().getForObject("http://localhost:5050/", String.class));
+				restTemplate.getForObject("http://localhost:5050/", String.class));
 	}
 
 }
